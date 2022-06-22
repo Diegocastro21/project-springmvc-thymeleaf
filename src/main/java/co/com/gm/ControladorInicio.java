8ba4cc35f6946a6e730743408bff2a72cca0d806
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-06-21 20:45:36 -0500

    Compartiendo informacion de una lista de personas

--- src/main/java/co/com/gm/ControladorInicio.java
+++ src/main/java/co/com/gm/ControladorInicio.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Slf4j
 public class ControladorInicio {
@@ -17,16 +21,36 @@ public class ControladorInicio {
     @GetMapping("/")
     public String inicio(Model model){
         String mensaje = "Empezando con Thymeleaf";
+
         Persona persona = new Persona();
         persona.setNombre("Diego");
         persona.setApellido("Castro");
         persona.setEmail("dcastro@gmail.com");
         persona.setTelefono("31056563834");
 
+        Persona persona2 = new Persona();
+        persona2.setNombre("petro");
+        persona2.setApellido("presidente");
+        persona2.setEmail("petrosky2022@gmail.com");
+        persona2.setTelefono("31387723434");
+
+        Persona persona3 = new Persona();
+        persona3.setNombre("Karla");
+        persona3.setApellido("Gomez");
+        persona3.setEmail("kgomez@gmail.com");
+        persona3.setTelefono("31293533445");
+
+        /*List<Persona> personas = new ArrayList<>();
+        personas.add(persona);
+        personas.add(persona2);
+        personas.add(persona3);*/
+
+        List<Persona> personas = Arrays.asList(persona, persona2, persona3);
         log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        //model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 
